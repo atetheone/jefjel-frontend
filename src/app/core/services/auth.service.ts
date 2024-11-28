7d9a820f5c390f0a6ef72583d9d43cb01d9fb8c4
atetheone <atevirran@gmail.com>
2024-11-28 13:25:45 +0000

    Enhance AuthService with login state tracking and type improvements

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,29 +1,36 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,  } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '#env/environment';
 import { BehaviorSubject, map, Observable } from 'rxjs';
-import { User } from '#types/user';
+import { User, UserResponse } from '#types/user';
 import { UserRegistrationResponse } from '#types/user_registration_response';
 import { LoginResponse } from '#types/login_response';
-import { LoginCredentials } from '../types/login_credentials';
+import { LoginRequest } from '#types/login_request';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private isLoggedInSubject = new BehaviorSubject<boolean>(false); // Tracks login state
+  public isLoggedIn$: Observable<boolean> = this.isLoggedInSubject.asObservable(); // Exposed as observable
   private apiUrl = environment.apiUrl;
   private userSubject = new BehaviorSubject<any>(null);
   public user$ = this.userSubject.asObservable();
 
+  private isLoggedIn: boolean = false;
+
   constructor(
     private http: HttpClient
-  ) { }
+  ) { 
+    const token = localStorage.getItem('token');
+    this.isLoggedInSubject.next(!!token);
+  }
 
-  registerUser(user: User) {
+  registerUser(user: User): Observable<UserRegistrationResponse> {
     return this.http.post<UserRegistrationResponse>(`${this.apiUrl}/auth/register`, user);
   }
 
-  login({ email, password }: LoginCredentials): Observable<any> {
+  login({ email, password }: LoginRequest): Observable<LoginResponse> {
     return this.http
       .post<LoginResponse>(`${this.apiUrl}/auth/login`, { email, password })
       .pipe(
@@ -31,14 +38,15 @@ export class AuthService {
           if (response.success) { 
             localStorage.setItem('token', response.token);
             this.userSubject.next(response.data);
+            this.isLoggedInSubject.next(true);
           }
           return response;
         })
       )
   }
 
-  getUser(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/me`).pipe(
+  getUser(): Observable<UserResponse> {
+    return this.http.get<UserResponse>(`${this.apiUrl}/me`).pipe(
       map((user) => {
         this.userSubject.next(user);
         return user;
@@ -49,5 +57,10 @@ export class AuthService {
   logout() {
     localStorage.removeItem('token');
     this.userSubject.next(null);
+    this.isLoggedInSubject.next(false)
+  }
+
+  isAuthenticated(): boolean {
+    return this.isLoggedInSubject.value;
   }
 }
