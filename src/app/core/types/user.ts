a81939031ea6039b8247cc31737926f6b46afb98
atetheone <atevirran@gmail.com>
2024-11-28 13:23:09 +0000

    Enhance User and UserResponse interfaces to include additional user details and roles

--- src/app/core/types/user.ts
+++ src/app/core/types/user.ts
@@ -1,5 +1,26 @@
+import { Role, RoleResponse } from './role'
+
 export interface User {
-  username: string;
-  email: string;
-  password: string;
+  id: number
+  username: string
+  email: string
+  firstName: string
+  lastName: string
+  tenantId: number
+  status: string
+  lastLoginAt?: string | null
+  roles: Role[]
+}
+
+export interface UserResponse {
+  id: number
+  username: string
+  email: string
+  firstName: string
+  lastName: string
+  tenantId?: number
+  createdAt: string
+  updatedAt: string
+  lastLoginAt?: string | null
+  roles: RoleResponse[]
 }
