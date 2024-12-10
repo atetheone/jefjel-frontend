58097c274304fb2ff1cef285a5bfe161bef3cfc2
atetheone <atevirran@gmail.com>
2024-12-10 14:44:45 +0000

    Add PermissionService with methods to get and create permissions

---
+++ src/app/core/services/permission.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { environment } from '#env/environment';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, map, Observable, throwError } from 'rxjs';
+import { PermissionResponse } from '#types/permission';
+import { ApiResponse } from '#types/api_response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PermissionService {
+  apiUrl = `${environment.apiUrl}/permissions`
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getPermissions(): Observable<ApiResponse<PermissionResponse[]>> {
+    return this.http.get<ApiResponse<PermissionResponse[]>>(this.apiUrl);
+  }
+
+  createPermission(perm: Partial<PermissionResponse>): Observable<ApiResponse<PermissionResponse>> {
+    return this.http.post<ApiResponse<PermissionResponse>>(this.apiUrl, perm);
+  }
+}
