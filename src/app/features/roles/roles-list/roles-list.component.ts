64b76a24ee5cbb53b5a5e304af6c4832bfd46c89
atetheone <atevirran@gmail.com>
2024-12-10 14:41:55 +0000

    Add tenant details component with template

---
+++ src/app/features/roles/roles-list/roles-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles-list',
+  imports: [],
+  templateUrl: './roles-list.component.html',
+  styleUrl: './roles-list.component.sass'
+})
+export class RolesListComponent {
+
+}
