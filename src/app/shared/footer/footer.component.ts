1ab9fb59eca4c7e1444effe751bcd131fccc990b
atetheone <atevirran@gmail.com>
2024-12-10 14:34:33 +0000

    Enhance footer component with new layout and styling; add quick links, customer service, and social media icons

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '#shared/material/material.module'
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.sass'
 })
