29937f8b2fcd170fb3f346e40e0cc528781a705e
atetheone <atevirran@gmail.com>
2024-12-16 11:47:35 +0000

    Update DashboardComponent: Add user welcome section with avatar and roles display

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,15 +1,11 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'
-import { MaterialModule } from '#shared/material/material.module'
-// import { CurrencyPipe } from '@angular/common'
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '#shared/material/material.module';
+import { Observable } from 'rxjs';
+import { AuthService } from '#services/auth.service';
+import { UserResponse } from '#types/user';
 
-interface DashboardStats {
-  title: string;
-  value: string;
-  change: number;
-  icon: string;
-  color: string;
-}
+
 
 @Component({
   selector: 'app-dashboard',
@@ -17,35 +13,15 @@ interface DashboardStats {
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.sass'
 })
-export class DashboardComponent {
-  dashboardStats: DashboardStats[] = [
-    {
-      title: 'Total Sales',
-      value: '$45,231',
-      change: 12.5,
-      icon: 'attach_money',
-      color: '#2196F3'
-    },
-    {
-      title: 'Orders',
-      value: '2,321',
-      change: 8.3,
-      icon: 'shopping_cart',
-      color: '#4CAF50'
-    },
-    {
-      title: 'Customers',
-      value: '1,456',
-      change: 15.7,
-      icon: 'people',
-      color: '#9C27B0'
-    },
-    {
-      title: 'Products',
-      value: '453',
-      change: 6.2,
-      icon: 'inventory',
-      color: '#FF9800'
-    }
-  ];
+export class DashboardComponent implements OnInit {
+  currentUser$: Observable<UserResponse>;
+
+  constructor(private authService: AuthService) {
+    this.currentUser$ = this.authService.user$
+  }
+
+  ngOnInit() {
+    this.authService.getUser().subscribe();
+  }
+
 }
