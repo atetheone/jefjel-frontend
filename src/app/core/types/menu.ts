20db974fc187c0b436e79528b67e876b0e5d0685
atetheone <atevirran@gmail.com>
2024-12-16 11:40:18 +0000

    Add additional properties to MenuItem interface and define MenuResponse type

--- src/app/core/types/menu.ts
+++ src/app/core/types/menu.ts
@@ -1,5 +1,16 @@
+import { ApiResponse } from './api_response';
+
 export interface MenuItem {
+  id: number;
   label: string;
   icon: string;
   route: string;
+  order?: number;
+  requiredPermissions?: string[];
+  children?: MenuItem[];
+  createdAt?: string;
+  updatedAt?: string;
 }
+
+
+export type MenuResponse = ApiResponse<MenuItem[]>;
