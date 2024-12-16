827caa3ca79b82bf1ddb621ea39e2d47fcb9afa6
atetheone <atevirran@gmail.com>
2024-12-16 11:46:11 +0000

    Update roles.routes.ts: Add permission guards for role access and actions

--- src/app/features/roles/roles.routes.ts
+++ src/app/features/roles/roles.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { RolesComponent } from './roles.component';
+import { permissionGuard } from '#guards/permission.guard';
 
 export const ROLES_ROUTES: Routes = [
   {
@@ -7,17 +8,22 @@ export const ROLES_ROUTES: Routes = [
     children: [
       {
         path: '',
-        component: RolesComponent
+        component: RolesComponent,
+        canActivate: [() => permissionGuard(['read:roles'])]
       },
       {
         path: 'create',
         loadComponent: () => import('./create-role/create-role.component')
-          .then(m => m.CreateRoleComponent)
+          .then(m => m.CreateRoleComponent),
+        canActivate: [() => permissionGuard(['create:roles'])],
+        data: { breadcrumb: 'Create role' }
       },
       {
         path: ':id',
         loadComponent: () => import('./role-details/role-details.component')
-          .then(m => m.RoleDetailsComponent)
+          .then(m => m.RoleDetailsComponent),
+        canActivate: [() => permissionGuard(['read:roles', 'update:roles'])],
+        data: { breadcrumb: 'Role details' }
       }
     ]
   }
