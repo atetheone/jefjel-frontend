7c2ec33363cb26d3bb1030cc93570a3344ffb381
atetheone <atevirran@gmail.com>
2024-12-16 11:42:33 +0000

    Refactor SidenavComponent to use Observable for menu items and improve async handling

--- src/app/layouts/admin-layout/sidenav/sidenav.component.ts
+++ src/app/layouts/admin-layout/sidenav/sidenav.component.ts
@@ -1,28 +1,34 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { CommonModule } from '@angular/common'
 import { Router, RouterModule } from '@angular/router'
+import { BehaviorSubject, Observable } from 'rxjs';
 import { MaterialModule } from '#shared/material/material.module'
 import { AuthService } from '#services/auth.service'
-import { MenuItem } from '#types/menu'
+import { MenuItem, MenuResponse } from '#types/menu'
 import { MenuService } from '#services/menu.service'
+
+
+
 @Component({
   selector: 'app-sidenav',
-  imports: [MaterialModule, RouterModule],
+  imports: [MaterialModule, RouterModule, CommonModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.sass'
 })
 export class SidenavComponent implements OnInit {
   @Input() isExpanded = true;
-  menuItems: MenuItem[] = [];
+  menuItems$: Observable<MenuItem[]>;
 
   constructor(
     private menuService: MenuService,
     // private authService: AuthService,
     // private router: Router
-  ) {}
-
-  ngOnInit(): void {
-    this.menuItems = this.menuService.getMenuItems();
+  ) {
+    this.menuItems$ = this.menuService.menu$;
   }
 
+  ngOnInit(): void {
+    this.menuService.loadMenu();
+  }
 
 }
