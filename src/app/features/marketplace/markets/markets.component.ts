8e9de55f7c69fce0b1ffccaef8b05e37eaf0b770
atetheone <atevirran@gmail.com>
2024-12-31 11:51:50 +0000

    Add MarketsComponent with template and unit tests

---
+++ src/app/features/marketplace/markets/markets.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-markets',
+  imports: [],
+  templateUrl: './markets.component.html',
+  styleUrl: './markets.component.sass'
+})
+export class MarketsComponent {
+
+}
