6880b33b9913bc03559441365049116629dcb7bd
atetheone <atevirran@gmail.com>
2025-01-09 10:01:32 +0000

    Add marketplace routes for navigation between markets and product details

---
+++ src/app/features/marketplace/marketplace.routes.ts
@@ -0,0 +1,33 @@
+import { Routes } from '@angular/router';
+import { MarketplaceComponent } from './marketplace.component';
+import { MarketsComponent } from './markets/markets.component';
+import { MarketDetailsComponent } from './market-details/market-details.component';
+import { MarketplaceProductDetailsComponent } from './components/product-details/product-details.component';
+
+export const MARKETPLACE_ROUTES: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: MarketsComponent,
+      },
+      {
+        path: ':id',
+        component: MarketDetailsComponent,
+        children: [
+          // { path: '', redirectTo: 'products', pathMatch: 'full' },
+          // { path: 'products', component: ProductListComponent },
+          
+          // { path: 'about', component: AboutComponent },
+          // { path: 'contact', component: ContactComponent }
+        ]
+      },
+      { 
+        path: ':marketId/products/:id', 
+        component: MarketplaceProductDetailsComponent,
+        data: { breadcrumb: 'Product Details' }
+      },
+    ]
+  }
+];
