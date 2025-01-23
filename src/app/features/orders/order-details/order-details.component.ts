404fdfd8e2d4f33ec24faaa127562257200cc484
atetheone <atevirran@gmail.com>
2025-01-23 16:21:59 +0000

    Implement notification listener in OrderDetailsComponent and manage subscription lifecycle

--- src/app/features/orders/order-details/order-details.component.ts
+++ src/app/features/orders/order-details/order-details.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '#shared/material/material.module';
 import { RouterModule, ActivatedRoute } from '@angular/router';
 import { OrderResponse } from '#types/order';
 import { OrderService } from '#shared/services/order.service';
-import { ToastService } from '#app/shared/services/toast.service';
-import { Observable, switchMap } from 'rxjs';
+import { ToastService } from '#shared/services/toast.service';
+import { NotificationService } from '#shared/services/notification.service'
+import { Observable, switchMap, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-order-details',
@@ -13,21 +14,33 @@ import { Observable, switchMap } from 'rxjs';
   templateUrl: './order-details.component.html',
   styleUrl: './order-details.component.sass'
 })
-export class OrderDetailsComponent implements OnInit {
+export class OrderDetailsComponent implements OnInit, OnDestroy {
+  notificationSubscription: Subscription | null = null
   order$: Observable<OrderResponse>;
   isProcessing = false;
+  orderId!: number;
 
   constructor(
     private orderService: OrderService,
     private route: ActivatedRoute,
-    private toastService: ToastService
+    private toastService: ToastService,
+    private notificationService: NotificationService
   ) {
     this.order$ = this.route.params.pipe(
-      switchMap(params => this.orderService.getOrder(params['id']))
+      switchMap(params => {
+        this.orderId = params['id'];
+        this.setupNotificationListener()
+        return this.orderService.getOrder(this.orderId)
+      })
     );
+
   }
 
   ngOnInit(): void {
+    this.notificationService.sayHello().subscribe({
+      next: (data) => console.log('Hello response:', data),
+      error: (error) => console.error('Socket error:', error)
+    });
   }
 
   updateOrderStatus(orderId: number, status: string) {
@@ -60,5 +73,18 @@ export class OrderDetailsComponent implements OnInit {
       case 'cancelled': return 'error';
       default: return 'warn';
     }
+  }
+
+  setupNotificationListener() {
+    this.notificationSubscription = this.notificationService.notifications$
+    .subscribe(notification => {
+      if (notification?.type === 'order_status' && notification.data.orderId === this.orderId) {
+        this.updateOrderStatus(notification.data, notification.data.status)
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    this.notificationSubscription?.unsubscribe()
   }
 }
